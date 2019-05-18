f9ae9b6ecc950c70d7c972de7809bba60ea43511
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2019-05-18 13:17:21 +0530

    email scheduling response/request

---
+++ spring-boot-quartz-email-scheduling/src/main/java/com/zeeshan/payload/ScheduleEmailRequest.java
@@ -0,0 +1,31 @@
+package com.zeeshan.payload;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import lombok.Data;
+
+@Data
+public class ScheduleEmailRequest {
+
+	@Email
+	@NotEmpty
+	private String email;
+
+	@NotEmpty
+	private String subject;
+
+	@NotEmpty
+	private String body;
+
+	@NotNull
+	private LocalDateTime dateTime;
+
+	@NotNull
+	private ZoneId timeZone;
+
+}
---
+++ spring-boot-quartz-email-scheduling/src/main/java/com/zeeshan/payload/ScheduleEmailResponse.java
@@ -0,0 +1,26 @@
+package com.zeeshan.payload;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.Data;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
+public class ScheduleEmailResponse {
+	private boolean success;
+	private String jobId;
+	private String jobGroup;
+	private String message;
+
+	public ScheduleEmailResponse(boolean success, String message) {
+		this.success = success;
+		this.message = message;
+	}
+
+	public ScheduleEmailResponse(boolean success, String jobId, String jobGroup, String message) {
+		this.success = success;
+		this.jobId = jobId;
+		this.jobGroup = jobGroup;
+		this.message = message;
+	}
+}
